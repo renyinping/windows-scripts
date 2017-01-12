26adf4b37fc2daf9ba0f4bf17704b3df4011a2c4
yinping <yp_ren@hotmail.com>
2017-01-12 19:11:24 +0800

    fix 修复找不到文件的问题

--- qs.bat
+++ qs.bat
@@ -1,22 +1,25 @@
 @echo off
+set OLD_DIR=%CD%
+cd %~dp0
+echo %CD%
 
 ::APP_NAME 应用快捷名
 ::CFG_FILE 快捷启动命令配置文件
 set APP_NAME=%~1
 set CFG_FILE=qs.conf
 
-if NOT EXIST %CFG_FILE% echo 找不到配置文件 & pause > nul & goto :eof
+if NOT EXIST %CFG_FILE% echo 找不到配置文件 & pause > nul & goto :quit
 
 ::循环读取快捷启动命令配置文件的行，发现匹配的快捷启动命令字符串，就执行start_app函数
 for /f "eol=; tokens=1-4 delims=," %%a in (%CFG_FILE%) do (
-	if "%APP_NAME%" == "%%a" call :start_app "%%b" "%%c" "%%d" & goto :eof
+	if "%APP_NAME%" == "%%a" call :start_app "%%b" "%%c" "%%d" & goto :quit
 )
 echo 找不到快捷应用:%APP_NAME%
 echo 赶紧去添加一个
 
 ::任意键退出
 pause > nul
-goto :eof
+goto :quit
 
 ::启动应用软件函数
 ::EXE_NAME 可执行文件名
@@ -32,4 +35,8 @@ goto :eof
 	@echo on
 	start /B "%APP_NOTE%" "%EXE_NAME%"
 	@echo off
+	goto :quit
+
+:quit
+	cd %OLD_DIR%
 	goto :eof
